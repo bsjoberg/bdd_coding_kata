f662d2875899e136bc4984652e83aa2eec533bf8
brian <brian_sjoberg@yahoo.com>
2019-08-06 11:06:11 -0400

    Initial commit

---
+++ src/test/java/fun/bdd/Stepdefs.java
@@ -0,0 +1,11 @@
+package fun.bdd;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+
+}
